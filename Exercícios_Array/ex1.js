cefe08613100e53ce264dcea158cd445a2a0acf0
Thi-ss <favorettoemilio@gmail.com>
2026-06-11 11:39:50 -0300

    Add atividade 1

---
+++ Exercícios_Array/ex1.js
@@ -0,0 +1,6 @@
+//Lista de 5 nomes
+let nomes = [["Thiago", "Santos Silva"], ["Kevin ","Goulart Trevisan"], ["Pamela", "Carvalho"], ["Gabriela", "Gimenes"], ["Maria", "Eduarda Rosa"]];
+
+console.log (nomes);
+console.log (nomes[0]);
+console.log (nomes [4]);
